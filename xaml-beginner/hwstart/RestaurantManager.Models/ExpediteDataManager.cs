6a6d7c7623d5a8bf7d0a31f52536dabb714bc020
joreava <joreava@gmail.com>
2016-01-10 18:55:02 +0100

    Homerwork #1

---
+++ xaml-beginner/hwstart/RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+
+
+
+        protected override void OnDataLoaded()
+        {
+            OnPropertyChanged("OrderItems");
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; } 
+        }
+    }
+}
