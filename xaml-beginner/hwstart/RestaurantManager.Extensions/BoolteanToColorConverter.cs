003130672cd31ef49d50c782e93db65e15cce9a9
joreava <joreava@gmail.com>
2016-01-14 23:26:26 +0100

    project extensions

---
+++ xaml-beginner/hwstart/RestaurantManager.Extensions/BoolteanToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Colors.Red : Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw  new NotImplementedException();
+        }
+    }
+}
