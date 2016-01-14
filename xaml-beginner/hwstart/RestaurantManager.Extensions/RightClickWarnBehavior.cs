98e4ef604dbd7d888e0eed937d678c6ba666021b
joreava <joreava@gmail.com>
2016-01-15 00:04:24 +0100

    behavior

---
+++ xaml-beginner/hwstart/RestaurantManager.Extensions/RightClickWarnBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickWarnBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+
+        public string Title { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+            }
+        }
+
+        public void Detach()
+        {
+            if (this.AssociatedObject != null && this.AssociatedObject is Page)
+            {
+                this.AssociatedObject = null;
+                (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+            }
+        }
+
+        private void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            new MessageDialog(this.Message, this.Title).ShowAsync();
+        }
+    }
+}
